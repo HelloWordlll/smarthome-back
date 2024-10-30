e2196f8e9ee2a9f4bd87f621cdfb9644bfef9146
helloword <132552268+HelloWordlll@users.noreply.github.com>
2024-10-31 01:04:41 +0800

    获取设备列表

--- src/main/java/com/hmall/smarthome/controller/IotController.java
+++ src/main/java/com/hmall/smarthome/controller/IotController.java
@@ -2,20 +2,28 @@ package com.hmall.smarthome.controller;
 
 
 import com.hmall.smarthome.common.BaseResponse;
+import com.hmall.smarthome.entry.vo.DeviceVO;
+import com.hmall.smarthome.server.IotService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @Slf4j
 @RestController
 @RequestMapping("/iot")
+@RequiredArgsConstructor
 public class IotController {
+
+    private final IotService iotService;
 
     @GetMapping("/list")
     public BaseResponse list(){
-
-        return BaseResponse.success();
+        List<DeviceVO> list = iotService.getList();
+        return BaseResponse.success(list);
     }
 }
